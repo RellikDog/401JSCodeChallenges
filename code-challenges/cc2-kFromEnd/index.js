615072c4b7c7c4b39dfebfdfc9e29d121a187336
RickBell <richard.belamy123@gmail.com>
2019-03-03 19:30:05 -0800

    cc7/2 done

--- code-challenges/cc2-kFromEnd/index.js
+++ code-challenges/cc2-kFromEnd/index.js
@@ -2,4 +2,5 @@
 
 const LinkedList = require('./lib/linked-list');
 const Node = require('./lib/newNode');
+const measureList = require('./lib/measureList');
 
--- code-challenges/cc2-kFromEnd/lib/linked-list.js
+++ code-challenges/cc2-kFromEnd/lib/linked-list.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Node = require('./newNode');
+const measureList = require('./measureList');
 
 class LinkedList {
     constructor(){
@@ -18,6 +19,29 @@ class LinkedList {
                 current = current.next;
             }
             current.next = newNode;
+        }
+    }
+    valueFromEnd(value){
+        const reg = new RegExp(/\d+/g);
+        if(reg.test(value)){
+            let acc = 0;
+            let current = this.head;
+            while(current.next){
+                acc++;
+                current = current.next;
+            }
+            let searchIndex = acc - value;
+            if(searchIndex >= 0){
+                let searchResult = this.head;
+                for(let i = 0; i < searchIndex; i++){
+                    searchResult = searchResult.next;
+                }
+                return searchResult.value;
+            }else{
+                return 'Error-the linked list is too short';
+            }
+        }else{
+            return 'must input a number';
         }
     }
 }
