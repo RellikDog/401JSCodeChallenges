c23b25e5523d11cdf7ffbe5a9f82f3379b0fb28f
RickBell <richard.belamy123@gmail.com>
2019-03-31 14:51:36 -0700

    finishing up

--- code-challenges/cc8-ll_merge/__tests__/merge.test.js
+++ code-challenges/cc8-ll_merge/__tests__/merge.test.js
@@ -14,11 +14,17 @@ describe('make stuff', () =>{
 });
 
 describe('mergeLL tests', () => {
+    let LLAlpha = new LinkedList();
+    let LLOmega = new LinkedList();
+    LLOmega.insertAtTail('gonna still break');
+    test('empty linked lists throws an error', ()=> {
+        expect(mergeLL(LLAlpha, LLOmega)).toEqual('Either 1 or both linked lists are empty');
+    });
     let LLOne = new LinkedList();
+    let LLTwo = new LinkedList();
     LLOne.insertAtTail('a');
     LLOne.insertAtTail('b');
     LLOne.insertAtTail('c');
-    let LLTwo = new LinkedList();
     LLTwo.insertAtTail('1');
     LLTwo.insertAtTail('2');
     LLTwo.insertAtTail('3');
@@ -27,5 +33,34 @@ describe('mergeLL tests', () => {
         expect(LLOne.head.next.value).toEqual('1');
         expect(LLOne.head.next.next.value).toEqual('b');
         expect(LLOne.head.next.next.next.value).toEqual('2');
+    });
+    let testLL1 = new LinkedList();
+    let testLL2 = new LinkedList();
+    testLL1.insertAtTail('ro');
+    testLL1.insertAtTail('sham');
+    testLL1.insertAtTail('bo');
+    testLL2.insertAtTail('you lose');
+    mergeLL(testLL1, testLL2);
+    test('test when the first ll is longer', () => {
+        expect(testLL1.head.value).toEqual('ro');
+        expect(testLL1.head.next.value).toEqual('you lose');
+        expect(testLL1.head.next.next.next.value).toEqual('bo');
+    });
+    let testLLA = new LinkedList();
+    let testLLB = new LinkedList();
+    testLLA.insertAtTail('smite');
+    testLLA.insertAtTail('you');
+    testLLB.insertAtTail('club');
+    testLLB.insertAtTail('will');
+    testLLB.insertAtTail('enjoy');
+    testLLB.insertAtTail('it');
+    mergeLL(testLLA, testLLB);
+    test('when the second LL is longer', () => {
+        expect(testLLA.head.value).toEqual('smite');
+        expect(testLLA.head.next.value).toEqual('club');
+        expect(testLLA.head.next.next.value).toEqual('you');
+        expect(testLLA.head.next.next.next.value).toEqual('will');
+        expect(testLLA.head.next.next.next.next.value).toEqual('enjoy');
+        expect(testLLA.head.next.next.next.next.next.value).toEqual('it');
     });
 });
--- code-challenges/cc8-ll_merge/ll-merge.js
+++ code-challenges/cc8-ll_merge/ll-merge.js
@@ -4,15 +4,21 @@ const Node = require('./lib/newNode');
 const LinkedList = require('./lib/linkedList');
 
 function mergeLL(ll1, ll2){
-    let ll1Cur = ll1.head;
-    let ll2Cur = ll2.head;
-    while(ll1Cur && ll2Cur){
-        let ref = ll1Cur.next;
-        let ref2 = ll2Cur.next;
-        ll1Cur.next = ll2Cur;
-        ll2Cur.next = ref;
-        ll1Cur = ref;
-        ll2Cur = ref2;
+    if(ll1.head && ll2.head){
+        let ll1Cur = ll1.head;
+        let ll2Cur = ll2.head;
+        while(ll1Cur && ll2Cur){
+            let ref = ll1Cur.next;
+            let ref2 = ll2Cur.next;
+            ll1Cur.next = ll2Cur;
+            if(ref){
+                ll2Cur.next = ref;
+            }
+            ll1Cur = ref;
+            ll2Cur = ref2;
+        }
+    }else {
+        return 'Either 1 or both linked lists are empty';
     }
 }
 
